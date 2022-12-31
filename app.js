07770fa80f3db08b5a45eaabc0bbfbb5d9a69fd5
kvbaurb99 <kvbaurbanski2@gmail.com>
2022-12-31 12:37:36 +0100

    clear function and calculate fixed

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ let current = '';
 let prev = '';
 let operator = ''; 
 
+// 2. Listeners
+
 // displaying number depending on clicked button
 numberButtons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -31,8 +33,16 @@ equalButton.addEventListener('click', () => {
     }
 })
 
+clearButton.addEventListener('click', clear);
+
+// 3. Calculation functions
+
 // function for displaying number only if length of this number is shorter or equal for 16
 function displayNumber(num) {
+    if (prev !== '' && current !== '' && operator === '') {
+        prev = '';
+        currentCalc.textContent = current;
+    }
     if (current.length <= 16) {
         current += num;
         currentCalc.textContent = current;
@@ -41,54 +51,61 @@ function displayNumber(num) {
 
 // function for displaying operator value after entering number
 function displayOperator(oper) {
-    operator = oper;
-    prev = current;
-    prevCalc.textContent = `${prev} ${operator}`;
+    if (prev === '') {
+        prev = current;
+        checkOperator(oper);
+    } else if (current === '') {
+        checkOperator(oper);
+    } else {
+        calculate();
+        operator = oper;
+        currentCalc.textContent = '0';
+        prevCalc.textContent = prev + ' ' + operator;
+    }
+}
+
+function checkOperator(str) {
+    operator = str;
+    prevCalc.textContent = prev + ' ' + operator;
+    currentCalc.textContent = '0';
     current = '';
-    currentCalc.textContent = '';    
 }
 
 
 // calculate function with operators switching(add, substract, multiply and divide), **when you divide by 0 function will throw error on displaying screen** 
 function calculate() {
-    prev === Number(prev);
+    prev = Number(prev);
     current = Number(current);
 
-    switch(operator) {
-        case '+':
-            prev += current;
-            break;
-        case '-':
-            prev -= current;
-            break;
-        case '×':
-            prev *= current;
-            break;
-        case '÷':
-            if (current === 0) {
-                prev = 'Cannot divide by 0!!!';
-                setResult();
-                return;
-            } else {
-                prev /= current;
-            } 
-            break;
+  if (operator === "+") {
+    prev += current;
+  } else if (operator === "-") {
+    prev -= current;
+  } else if (operator === "x") {
+    prev *= current;
+  } else if (operator === "/") {
+    if (current <= 0) {
+      prev = "Error";
+      setResult();
+      return;
     }
-    // result will be rounded
-    prev = roundNumber(prev);
-    prev = prev.toString();
-    setResult();
+    prev /= current;
+  }
+  prev = roundNumber(prev);
+  prev = prev.toString();
+  setResult();
 }
 
 // function for setting result on current calculation screen, if result is longer than 16 it will throw three dots after result.
 function setResult() {
-    prevCalc.textContent = '';
-    operator = '';
     if (prev.length <= 16) {
         currentCalc.textContent = prev;
     } else {
         currentCalc.textContent = prev.slice(0, 16) + '...';
     }
+    prevCalc.textContent = '';
+    operator = '';
+    current = '';
 }
 
 
@@ -97,3 +114,10 @@ function roundNumber(num) {
     return Math.round(num * 100000) / 100000;
 }
 
+function clear() {
+    current = '';
+    prev = '';
+    operator = '';
+    currentCalc.textContent = '0';
+    prevCalc.textContent = '';
+}
