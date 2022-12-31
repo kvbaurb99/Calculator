5c92eafc5e4dcc743d60d81b3de0cd8353e4ac93
kvbaurb99 <kvbaurbanski2@gmail.com>
2022-12-31 12:22:42 +0100

    calculation functions

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ let current = '';
 let prev = '';
 let operator = ''; 
 
+// displaying number depending on clicked button
 numberButtons.forEach(button => {
     button.addEventListener('click', (e) => {
         displayNumber(e.target.textContent);
     })
 })
 
+// displaying operator depending on clicked button
+operatorButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        displayOperator(e.target.textContent);
+    })
+})
+
+// on equal button click function will calculate result
+equalButton.addEventListener('click', () => {
+    if (current !== '' && prev !== '') {
+        calculate();
+    }
+})
+
+// function for displaying number only if length of this number is shorter or equal for 16
 function displayNumber(num) {
     if (current.length <= 16) {
         current += num;
@@ -23,20 +39,61 @@ function displayNumber(num) {
     }
 }
 
-// 2. Basic calculations
-
-const add = (a, b) => {
-    return a + b;
+// function for displaying operator value after entering number
+function displayOperator(oper) {
+    operator = oper;
+    prev = current;
+    prevCalc.textContent = `${prev} ${operator}`;
+    current = '';
+    currentCalc.textContent = '';    
 }
 
-const substract = (a, b) => {
-    return a - b;
+
+// calculate function with operators switching(add, substract, multiply and divide), **when you divide by 0 function will throw error on displaying screen** 
+function calculate() {
+    prev === Number(prev);
+    current = Number(current);
+
+    switch(operator) {
+        case '+':
+            prev += current;
+            break;
+        case '-':
+            prev -= current;
+            break;
+        case '×':
+            prev *= current;
+            break;
+        case '÷':
+            if (current === 0) {
+                prev = 'Cannot divide by 0!!!';
+                setResult();
+                return;
+            } else {
+                prev /= current;
+            } 
+            break;
+    }
+    // result will be rounded
+    prev = roundNumber(prev);
+    prev = prev.toString();
+    setResult();
 }
 
-const multiply = (a, b) => {
-    return a * b;
+// function for setting result on current calculation screen, if result is longer than 16 it will throw three dots after result.
+function setResult() {
+    prevCalc.textContent = '';
+    operator = '';
+    if (prev.length <= 16) {
+        currentCalc.textContent = prev;
+    } else {
+        currentCalc.textContent = prev.slice(0, 16) + '...';
+    }
 }
 
-const divide = (a, b) => {
-    return a / b;
+
+// function for rounding result 
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
 }
+
